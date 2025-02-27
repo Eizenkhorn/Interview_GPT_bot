1fb998c09ba1f5bd708eba9f1bc3f315893c35f6
Eizenkhorn <Sasha.titov2004@mail.ru>
2025-02-28 02:27:12 +0300

    Initial commit

---
+++ src/main/java/org/example/interview_gpt_bot/InterviewGptBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.interview_gpt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewGptBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewGptBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/interview_gpt_bot/InterviewGptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.interview_gpt_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewGptBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
